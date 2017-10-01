adc70186e4960946ded8e534094a21c57b143cea
zaraavoyan <31899165+zaraavoyan@users.noreply.github.com>
2017-09-30 23:20:03 -0700

    Create sketch6.js

---
+++ sketch6.js
@@ -0,0 +1,24 @@
+rectMode (CENTER);
+var t = 10;
+var r = 50;
+var p = 0;
+noFill();
+
+
+
+
+
+while (r<340)
+{
+  {
+    var t = 10;
+var p = 0;
+
+  while (p < 40) {
+    rect (r, 170, t, t);
+    t = t + 10;
+    p = p + 10;  
+  }
+   r = r + 75;       
+  }
+}
