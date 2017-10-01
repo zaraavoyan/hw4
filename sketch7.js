8fd3fb3cab6b4f3aa094569cd26c7c7577d0a256
zaraavoyan <31899165+zaraavoyan@users.noreply.github.com>
2017-09-30 23:34:59 -0700

    Create sketch7.js

---
+++ sketch7.js
@@ -0,0 +1,17 @@
+background (0);
+ellipseMode (CENTER);
+var y = 20;
+while (y < 340) {
+  
+ {
+    fill (255);
+    noStroke();
+    var x = 20;
+
+    while (x < 340) {
+    ellipse (x, y, 20);
+    x = x + 30;
+  }
+    y = y + 30;
+}
+}
