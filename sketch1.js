c4a241e697f60d0494d704353cba7e7d14166237
zaraavoyan <31899165+zaraavoyan@users.noreply.github.com>
2017-09-30 23:09:51 -0700

    Create sketch1.js

---
+++ sketch1.js
@@ -0,0 +1,14 @@
+var y = 10; 
+while ( y < height ) { 
+ line (0, y, width, y);
+ y = y + 10;
+}
+
+var w = 10;
+
+ while ( w < height, w < width) {
+  stroke(0);
+  noFill();
+ellipse (170, 170, w, w);
+  w = w + 10;
+ }
